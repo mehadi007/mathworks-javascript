1e5e9fa4a664c2a2e3c755dba7b9d51adb7186aa
John Paxton <johnpaxton@gmail.com>
2022-04-19 09:14:17 -0400

    Add data

---
+++ data/students.js
@@ -0,0 +1,154 @@
+const data = {
+    "students": [
+      {
+        "id": 0,
+        "location": "US",
+        "firstName": "Daniel",
+        "lastName": "Kotlinski"
+      },
+      {
+        "id": 1,
+        "location": "US",
+        "firstName": "Katrina",
+        "lastName": "Wallace"
+      },
+      {
+        "id": 2,
+        "location": "US",
+        "firstName": "Mary",
+        "lastName": "Wishart"
+      },
+      {
+        "id": 3,
+        "location": "US",
+        "firstName": "Steven",
+        "lastName": "Portillo"
+      },
+      {
+        "id": 4,
+        "location": "US",
+        "firstName": "Alex",
+        "lastName": "Mazzarese"
+      },
+      {
+        "id": 5,
+        "location": "US",
+        "firstName": "Caroline",
+        "lastName": "Manghan"
+      },
+      {
+        "id": 6,
+        "location": "US",
+        "firstName": "Joseph",
+        "lastName": "Travers"
+      },
+      {
+        "id": 7,
+        "location": "US",
+        "firstName": "Ambioris",
+        "lastName": "Lora"
+      },
+      {
+        "id": 8,
+        "location": "US",
+        "firstName": "Adam",
+        "lastName": "Audet"
+      },
+      {
+        "id": 9,
+        "location": "Europe",
+        "firstName": "Charlie",
+        "lastName": "Nathan"
+      },
+      {
+        "id": 10,
+        "location": "Europe",
+        "firstName": "Jun Hao",
+        "lastName": "Chia"
+      },
+      {
+        "id": 11,
+        "location": "Europe",
+        "firstName": "Callum",
+        "lastName": "Ogle"
+      },
+      {
+        "id": 12,
+        "location": "Europe",
+        "firstName": "Rahul",
+        "lastName": "Whig"
+      },
+      {
+        "id": 13,
+        "location": "Europe",
+        "firstName": "Wiktoria",
+        "lastName": "Fiolek"
+      },
+      {
+        "id": 14,
+        "location": "US",
+        "firstName": "Jacob",
+        "lastName": "Whiteman"
+      },
+      {
+        "id": 15,
+        "location": "US",
+        "firstName": "Kaitlin",
+        "lastName": "Mullins"
+      },
+      {
+        "id": 16,
+        "location": "US",
+        "firstName": "Maria",
+        "lastName": "Ringes"
+      },
+      {
+        "id": 17,
+        "location": "US",
+        "firstName": "Peter",
+        "lastName": "Baker"
+      },
+      {
+        "id": 18,
+        "location": "US",
+        "firstName": "Brianna",
+        "lastName": "Fahrenkopf"
+      },
+      {
+        "id": 19,
+        "location": "US",
+        "firstName": "Philip",
+        "lastName": "Kopita"
+      },
+      {
+        "id": 20,
+        "location": "US",
+        "firstName": "Christopher",
+        "lastName": "Zarba"
+      },
+      {
+        "id": 21,
+        "location": "Canada",
+        "firstName": "Abdul",
+        "lastName": "Qureshi"
+      },
+      {
+        "id": 22,
+        "location": "Canada",
+        "firstName": "Seena",
+        "lastName": "Mathew"
+      },
+      {
+        "id": 23,
+        "location": "Canada",
+        "firstName": "Christopher",
+        "lastName": "Gritter"
+      },
+      {
+        "id": 24,
+        "location": "Canada",
+        "firstName": "Baltej",
+        "lastName": "Toor"
+      }
+    ]
+};
